2c68d6073d92f7c41a2ee99cf776f532597c4005
prtrenovationsinc-stack <prtrenovationsinc@gmail.com>
2026-03-29 00:29:13 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,6 @@
 // src/pages/Index.tsx
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
@@ -8,7 +10,7 @@ import ProjectsSection from "@/components/ProjectsSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import ProcessSection from "@/components/ProcessSection";
-import AreasWeServe from "@/components/AreasWeServe"; // <-- New Import
+import AreasWeServe from "@/components/AreasWeServe";
 import QuoteForm from "@/components/QuoteForm";
 import FAQSection from "@/components/FAQSection";
 import SocialSection from "@/components/SocialSection";
@@ -16,25 +18,45 @@ import FinalCTA from "@/components/FinalCTA";
 import Footer from "@/components/Footer";
 import FloatingCTA from "@/components/FloatingCTA";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <HeroSection />
-    <TrustStrip />
-    <ServicesSection />
-    <ProjectsSection />
-    <WhyChooseUs />
-    <TestimonialsSection />
-    <ProcessSection />
-    {/* Dropped in right above the quote form */}
-    <AreasWeServe />
-    <QuoteForm />
-    <FAQSection />
-    <SocialSection />
-    <FinalCTA />
-    <Footer />
-    <FloatingCTA />
-  </>
-);
+const Index = () => {
+  const location = useLocation();
+
+  // This automatically scrolls the user to the right section when they click a button from the /links page
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) {
+        // A tiny 150 millisecond delay ensures the page is fully drawn before scrolling
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 150);
+      }
+    } else {
+      // If there is no hash, make sure they start at the very top of the page
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <TrustStrip />
+      <ServicesSection />
+      <ProjectsSection />
+      <WhyChooseUs />
+      <TestimonialsSection />
+      <ProcessSection />
+      <AreasWeServe />
+      <QuoteForm />
+      <FAQSection />
+      <SocialSection />
+      <FinalCTA />
+      <Footer />
+      <FloatingCTA />
+    </>
+  );
+};
 
 export default Index;
